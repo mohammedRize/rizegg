8a7b5b3d388130abb44d06b76cc8b965972aa422
Mohammed Saud Alharbi <al3meedforsale@gmail.com>
2025-05-14 13:38:06 +0300

    deploy

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   devIndicators: false,
+  output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
